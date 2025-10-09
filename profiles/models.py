9347d1a0a7c31f2f12c90a67fcd7a563cbaf83e6
sa-hil-29 <sahilsaifi2394@gmail.com>
2025-10-09 11:55:35 +0530

    Second Commit- Student model created

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from accounts.models import User
 
-# Create your models here.
+
+class Student(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    roll_number = models.CharField(max_length=20, unique=True)
+    cgpa = models.FloatField()
+    branch = models.CharField(max_length=50)
+    resume_url = models.FileField(upload_to="resumes/", blank=True)
+    skills = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.user.username
