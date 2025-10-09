b479057043727d726e44e657fbf0c105f65d9ce9
sa-hil-29 <sahilsaifi2394@gmail.com>
2025-10-09 11:57:10 +0530

    Second Commit- Jobs Model added

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Job(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    company_name = models.CharField(max_length=100)
+    eligibility_criteria = models.TextField(
+        blank=True
+    )  # e.g., "CGPA > 7.0, Branch: CS"
+    deadline = models.DateField()
+    status = models.CharField(max_length=20, default="open")
+
+    def __str__(self):
+        return self.title
