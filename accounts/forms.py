78e1f68ebf93a3ca2de83875f8756e642aac29d8
sa-hil-29 <sahilsaifi2394@gmail.com>
2025-10-09 12:02:56 +0530

    Third Commit- Registration and Login Forms added

---
+++ accounts/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import User
+
+
+class RegistrationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    role = forms.ChoiceField(choices=User.ROLE_CHOICES)
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2", "role")
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data["email"]
+        user.role = self.cleaned_data["role"]
+        if commit:
+            user.save()
+        return user
+
+
+class LoginForm(forms.Form):
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput)
