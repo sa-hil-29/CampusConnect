700c5d332388a17f075e62e25813acfbd4ae19b1
sa-hil-29 <sahilsaifi2394@gmail.com>
2025-10-09 11:58:15 +0530

    Second Commit- Applications Model added

--- applications/models.py
+++ applications/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from profiles.models import Student
+from jobs.models import Job
 
-# Create your models here.
+
+class Application(models.Model):
+    STATUS_CHOICES = (
+        ("applied", "Applied"),
+        ("shortlisted", "Shortlisted"),
+        ("selected", "Selected"),
+        ("rejected", "Rejected"),
+    )
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    job = models.ForeignKey(Job, on_delete=models.CASCADE)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="applied")
+    application_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.student.user.username} - {self.job.title}"
